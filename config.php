f0c2aa97906617230b1df49eb92d43a2845be614
avinash <avinash.yashco@gmx.com>
2019-11-18 16:56:39 +0530

    fourth  commit

--- config.php
+++ config.php
@@ -18,7 +18,7 @@ $CFG->dboptions = array (
   'dbcollation' => 'utf8_general_ci',
 );
 
-echo 'h123456666i';
+echo 'h123456666i98+';
 
 $CFG->wwwroot   = 'https://miss.moe/training';
 $CFG->dataroot  = '/home/admin/web/miss.moe/public_html/moodledata';
